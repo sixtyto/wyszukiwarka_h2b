5d309e38e16640ee186c39038b700e0c692c41a6
Piotr Czajkowski <sixtyto@gmail.com>
2020-09-09 21:06:04 +0200

    add basic styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import React, { useState, useEffect } from "react";
 import Unsplash, { toJson } from "unsplash-js";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Image from "react-bootstrap/Image";
+
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const APP_ACCESS_KEY = "q2mG3uuehVG6zS8CkSTPDyi4XkYmvs0F2hZY3VeOYPE";
 const APP_SECRET_KEY = "41xKc3ET1WEoFZc2w2PGsaVZnOwA-xXejlEsclvY3yU";
@@ -18,11 +24,20 @@ function App() {
   }, []);
   console.log(images);
   return (
-    <ul>
-      {images.map((image) => (
-        <img key={image.id} src={image.urls.full} alt={image.alt_description} />
-      ))}
-    </ul>
+    <Container>
+      <Row>
+        {images.map((image) => (
+          <Col md={4}>
+            <Image
+              key={image.id}
+              src={image.urls.full}
+              alt={image.alt_description}
+              thumbnail
+            />
+          </Col>
+        ))}
+      </Row>
+    </Container>
   );
 }
 
