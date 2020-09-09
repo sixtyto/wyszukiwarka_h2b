3e5493ee2b74de5d1788510c49bea673374ae0a9
Piotr Czajkowski <sixtyto@gmail.com>
2020-09-09 20:53:38 +0200

    add photos list

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Unsplash, { toJson } from "unsplash-js";
+
+const APP_ACCESS_KEY = "q2mG3uuehVG6zS8CkSTPDyi4XkYmvs0F2hZY3VeOYPE";
+const APP_SECRET_KEY = "41xKc3ET1WEoFZc2w2PGsaVZnOwA-xXejlEsclvY3yU";
+
+const unsplash = new Unsplash({ accessKey: APP_ACCESS_KEY });
 
 function App() {
+  const [images, setImages] = useState([]);
+  useEffect(() => {
+    unsplash.search
+      .photos("dogs", 1, 10, { orientation: "portrait", color: "green" })
+      .then(toJson)
+      .then((json) => {
+        setImages([...json.results]);
+      });
+  }, []);
+  console.log(images);
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ul>
+      {images.map((image) => (
+        <img key={image.id} src={image.urls.full} alt={image.alt_description} />
+      ))}
+    </ul>
   );
 }
 
