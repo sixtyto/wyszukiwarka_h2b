b3ea8bb4638f637f3f1662e13269d92eb53d143a
Piotr Czajkowski <sixtyto@gmail.com>
2020-09-13 08:35:44 +0200

    moved gallery component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import React, { useState, useEffect } from "react";
 import Unsplash, { toJson } from "unsplash-js";
 import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import Image from "react-bootstrap/Image";
-import Button from "react-bootstrap/Button";
 
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-import VerticallyCenteredModal from "./components/VerticallyCenteredModal";
 import Hero from "./components/Hero";
 import SearchBar from "./components/SearchBar";
+import Gallery from "./components/Gallery";
 import options from "./options";
 
 const APP_ACCESS_KEY = "q2mG3uuehVG6zS8CkSTPDyi4XkYmvs0F2hZY3VeOYPE";
@@ -77,65 +73,17 @@ const App = () => {
         handleEnter={handleEnter}
         handleChange={handleChange}
       />
-      {hero ? null : (
-        <>
-          <Row>
-            <Col>
-              <div className={"gallery"}>
-                {images.map((image) => (
-                  <>
-                    <Button
-                      key={image.id}
-                      className={"gallery_item p-0"}
-                      variant="link"
-                      onClick={() => {
-                        setModalShow(true);
-                        setModalImage(image);
-                      }}
-                    >
-                      <Image
-                        src={image.urls.thumb}
-                        alt={image.alt_description}
-                        fluid
-                      />
-                    </Button>
-                  </>
-                ))}
-                <VerticallyCenteredModal
-                  image={modalImage}
-                  show={modalShow}
-                  onHide={() => setModalShow(false)}
-                />
-              </div>
-            </Col>
-          </Row>
-
-          <Row>
-            <Col xs={{ offset: 2 }}>
-              <div className="p-3">
-                <Button
-                  disabled={page === 1}
-                  variant={page === 1 ? "light" : "primary"}
-                  onClick={() => handlePage(-1)}
-                >
-                  Previous page
-                </Button>
-              </div>
-            </Col>
-            <Col xs={{ offset: 4 }}>
-              <div className="p-3">
-                <Button
-                  disabled={!nextPageResults}
-                  variant={!nextPageResults ? "light" : "primary"}
-                  onClick={() => handlePage(1)}
-                >
-                  Next page
-                </Button>
-              </div>
-            </Col>
-          </Row>
-        </>
-      )}
+      <Gallery
+        hero={hero}
+        images={images}
+        setModalShow={setModalShow}
+        setModalImage={setModalImage}
+        modalImage={modalImage}
+        modalShow={modalShow}
+        page={page}
+        handlePage={handlePage}
+        nextPageResults={nextPageResults}
+      />
     </Container>
   );
 };
---
+++ src/components/Gallery.js
@@ -0,0 +1,82 @@
+import React from "react";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Image from "react-bootstrap/Image";
+import Button from "react-bootstrap/Button";
+
+import VerticallyCenteredModal from "./VerticallyCenteredModal";
+
+const Gallery = (props) => {
+  const {
+    hero,
+    images,
+    setModalShow,
+    setModalImage,
+    modalImage,
+    modalShow,
+    page,
+    handlePage,
+    nextPageResults,
+  } = props;
+  return hero ? null : (
+    <>
+      <Row>
+        <Col>
+          <div className={"gallery"}>
+            {images.map((image) => (
+              <>
+                <Button
+                  key={image.id}
+                  className={"gallery_item p-0"}
+                  variant="link"
+                  onClick={() => {
+                    setModalShow(true);
+                    setModalImage(image);
+                  }}
+                >
+                  <Image
+                    src={image.urls.thumb}
+                    alt={image.alt_description}
+                    fluid
+                  />
+                </Button>
+              </>
+            ))}
+            <VerticallyCenteredModal
+              image={modalImage}
+              show={modalShow}
+              onHide={() => setModalShow(false)}
+            />
+          </div>
+        </Col>
+      </Row>
+
+      <Row>
+        <Col xs={{ offset: 2 }}>
+          <div className="p-3">
+            <Button
+              disabled={page === 1}
+              variant={page === 1 ? "light" : "primary"}
+              onClick={() => handlePage(-1)}
+            >
+              Previous page
+            </Button>
+          </div>
+        </Col>
+        <Col xs={{ offset: 4 }}>
+          <div className="p-3">
+            <Button
+              disabled={!nextPageResults}
+              variant={!nextPageResults ? "light" : "primary"}
+              onClick={() => handlePage(1)}
+            >
+              Next page
+            </Button>
+          </div>
+        </Col>
+      </Row>
+    </>
+  );
+};
+
+export default Gallery;
